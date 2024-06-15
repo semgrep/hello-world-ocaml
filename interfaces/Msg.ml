1fb8cd9ee1c2f4fae4748c5c689a61cd949ded2f
pad <yoann.padioleau@gmail.com>
2024-06-15 11:05:38 +0200

    add symlink

---
+++ interfaces/Msg.ml
@@ -0,0 +1,4 @@
+let greeting = "Hello World"
+
+let%test "size" =
+  String.length greeting = 11
--- src/lib/Msg.ml
+++ src/lib/Msg.ml
@@ -1,4 +1,1 @@
-let greeting = "Hello World"
-
-let%test "size" =
-  String.length greeting = 11
+../../interfaces/Msg.ml
