10993c356420f0f635e484f826a8c949678214e6
pad <pad@r2c.dev>
2024-06-28 10:35:54 +0200

    Print hello world in colors
    This is to help debug coloring issue for semgrep in windows
    
    test plan:
    make
    ./bin/hello-world
    in Unix and Windows

--- interfaces/Msg.ml
+++ interfaces/Msg.ml
@@ -1,4 +1,7 @@
-let greeting = "Hello World"
+module A = ANSITerminal
+
+let greeting =
+  A.sprintf [A.green] "Hello" ^ A.sprintf [A.blue] " World"
 
 let%test "size" =
   String.length greeting = 11
